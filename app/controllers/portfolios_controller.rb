a1e1ba964bffd6f336947ab7d54b3b9a0aeedaa0
tarikulislam <tarikulislam.tar@gmail.com>
2017-08-12 22:18:36 +0600

    Impemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -5,7 +5,7 @@ class PortfoliosController < ApplicationController
   end
 
   def new
-    @portfolio_items = Portfolio.new
+    @portfolio_item = Portfolio.new
   end
 
   def create 
@@ -15,10 +15,26 @@ class PortfoliosController < ApplicationController
       if @portfolio_item.save
        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.'}
       else
-       format.html { render :new }
+       format.html { render :new}
       end
     end
 
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update((params.require(:portfolio).permit(:title, :subtitle, :body)))
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
 end
