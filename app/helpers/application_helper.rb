3f668f146c27b92534f33fdd86142c1da1008f75
tarikulislam <tarikulislam.tar@gmail.com>
2017-08-24 13:07:08 +0600

    Implemented navigation helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,6 @@ module ApplicationHelper
     end
   end
 
-
  def source_helper(layout_name)
   if session[:source]
     greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout" 
@@ -21,5 +20,45 @@ module ApplicationHelper
  def copyright_generator
   DevcampViewTool::Renderer.copyright 'Tarikulislam', 'All rights reserved'
  end
+ 
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+      },
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 
 end
+
