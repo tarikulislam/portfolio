04b88970cc38e70a96e1e5b3369c9ad78f9ce871
tarikulislam <tarikulislam.tar@gmail.com>
2017-08-25 20:54:02 +0600

    Implemented persistent position updates for drag and drop interface

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,11 +1,18 @@
 class PortfoliosController < ApplicationController
-  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
+  before_action :set_portfolio_item, only: [:edit, :show, :update, :destroy]
   layout 'portfolio'
-  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :update, :edit]}, site_admin: :all
-
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all
   
   def index
     @portfolio_items = Portfolio.by_position
+  end
+
+  def sort
+    params[:order].each do |key, value|
+      Portfolio.find(value[:id]).update(position: value[:position])
+    end
+
+    render nothing: true
   end
 
   def angular
@@ -33,7 +40,6 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
@@ -69,5 +75,4 @@ class PortfoliosController < ApplicationController
   def set_portfolio_item
     @portfolio_item = Portfolio.find(params[:id])
   end
-
 end
