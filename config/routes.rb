d680c2aa837647f4e53708453726c8ac05ad6ed9
tarikulislam <tarikulislam.tar@gmail.com>
2017-08-30 22:45:04 +0600

    Completed comment feature with action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-   resources :portfolios, except: [:show] do
+  resources :portfolios, except: [:show] do
     put :sort, on: :collection
   end
   get 'angular-items', to: 'portfolios#angular'
@@ -16,7 +15,7 @@ Rails.application.routes.draw do
       get :toggle_status
     end
   end
-  
+
   mount ActionCable.server => '/cable'
 
   root to: 'pages#home'
